2309340307064bc55c49d4821de57f59299b8e99
mayssam-Alsakhen <mayssamalsakhen17@gmail.com>
2024-05-19 17:49:32 +0300

    first comit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/Components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
@@ -28,35 +28,35 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
      
-      keyframes: {
-        wiggle: {
-          '0%, 100%': { transform: 'rotate(-3deg)' },
-          '50%': { transform: 'rotate(3deg)' },
-        }
-      },
-      animation: {
-        'wiggle': 'wiggle 1s ease-in-out 3',
-        'spin': 'spin 10s linear infinite',
-      }
+      // keyframes: {
+      //   wiggle: {
+      //     '0%, 100%': { transform: 'rotate(-3deg)' },
+      //     '50%': { transform: 'rotate(3deg)' },
+      //   }
+      // },
+      // animation: {
+      //   'wiggle': 'wiggle 1s ease-in-out 3',
+      //   'spin': 'spin 10s linear infinite',
+      // }
 
+      colors:{
+        // 050d2b 11264a
+        'prime': "#050d2b",
+        'second': '#E5F0FF',
+        'secondDark':'#020c30',
+        'designing' : '#0288d1',
+        'pending': '#fcfd95',
+        'progress':'#e8c7fd',
+        'testing':'#b1f2ff',
+        'done':'#baffc5',
+        'important':'#ffcccc',
+        'importanttext':'#f16667',
+        'progresstext':'#cb40f5',
+        'pendingtext':'#fef24e',
+        'donetext':'#04c005',
+        'testingtext':'#35a8c0'
+      }
     },
-    colors:{
-      // 050d2b 11264a
-      'prime': "#050d2b",
-      'second': '#E5F0FF',
-      'secondDark':'#020c30',
-      'designing' : '#0288d1',
-      'pending': '#fcfd95',
-      'progress':'#e8c7fd',
-      'testing':'#b1f2ff',
-      'done':'#baffc5',
-      'important':'#ffcccc',
-      'importanttext':'#f16667',
-      'progresstext':'#cb40f5',
-      'pendingtext':'#fef24e',
-      'donetext':'#04c005',
-      'testingtext':'#35a8c0'
-    }
   },
   plugins: [],
 };
